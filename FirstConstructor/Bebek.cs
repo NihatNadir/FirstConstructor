edfdfbf3ae7cab55f1084ca85a005fca34e06863
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-09 23:30:55 +0300

    Add project files.

---
+++ FirstConstructor/Bebek.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConstructor
+{
+    internal class Bebek
+    {
+        /*
+        Bu örnekte sizlerden bir Bebek sınıfı tanımlamanız isteniyor.
+        Bebek sınıfının propertyleri -> Doğum Tarihi, Ad , Soyad
+        Bebek sınıfı için 2 adet constructor tanımlayınız.
+        1.si Default Constructor tarzında parametre almayan bir metot.
+        2.si Ad ve Soyad parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
+        2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
+        Tanımlama sonrasında her iki constructor ile de birer bebek nesnesi tanımlayarak özelliklerini konsol ekranına yazdırınız.
+        */
+        private string name; // name field
+        private string surName; // surName field
+        private DateTime birthdate; // birthdate field
+
+        public string Name // Name property
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string SurName // SurName property
+        {
+            get
+            {
+                return surName;
+            }
+            set
+            {
+                surName = value;
+            }
+        }
+
+        public DateTime Birthdate // Birthdate property
+        {
+            get
+            {
+                return birthdate;
+            }
+            set
+            {
+                birthdate = value;
+            }
+        }
+        public Bebek() // Default Constructor
+        {
+            Console.WriteLine("Ingaa");
+            birthdate = DateTime.Now;
+        }
+
+        public Bebek(string a,string b) // 2 string parametresi alan Constructor
+        {
+            Console.WriteLine("Ingaa");
+            birthdate = DateTime.Now;
+            name = a;
+            surName = b;
+        }
+
+        public void TellInfo()
+        {
+            Console.WriteLine($"Bebeğin adı {name} soyadı {surName} doğum tarihi {birthdate.ToString("dd.MM.yyyy")}"); // Çağrıldığı zaman nesne bilgilerini döner
+        }
+    }
+}
---
+++ FirstConstructor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace FirstConstructor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bebek newBaby = new Bebek(); // Default Constructor ile oluşturulan nesne
+            newBaby.Name = "Ahmet";
+            newBaby.SurName = "Yıldırım";          
+            newBaby.TellInfo(); // newBaby nesnesine ait bilgilerin geleceği metot
+
+            Bebek newBaby2 = new Bebek("Serhat", "Yıldız"); // 2 parametre alan Constructor ile oluşturulan nesne
+            newBaby2.TellInfo();  // newBaby2 nesnesine ait bilgilerin geleceği metot
+        }
+    }
+}
